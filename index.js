bebab60c4018cce9aea4cf0971485a9aac9bb6a0
Cosimo Curlante <cosimocurlante8@gmail.com>
2023-05-30 17:48:22 +0200

    "Test Push"

--- index.js
+++ index.js
@@ -6,5 +6,14 @@ const submit = document.querySelector("#btn_sub")
 form.addEventListener("submit" , (e) => {
     e.preventDefault()
     const d1 = document.querySelector("input[name='domanda_1']")
-    console.log(d1);
+    console.log(d1.value);
 })
+
+/**
+ * YOOOOOOOOOOOOOOO
+ * OOOOOOOOOOOOOOOOO
+ * 
+ * OOOOOOOOOOOOOOO
+ * 
+ * OOOOOOOOOO
+ */
