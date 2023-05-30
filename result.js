b90112aec56bb668194640aeb57a3a3b2cc045fd
Ka3sar <kaesaryt@gmail.com>
2023-05-30 20:19:17 +0200

    score update

--- result.js
+++ result.js
@@ -1,5 +1,5 @@
 const result = document.querySelector("#result");
 const search = new URLSearchParams(window.location.search);
 const score = search.get("score"); 
-result.textContent = "Bro, il tuo score è: "+score+"/39"
+result.textContent = "Bro, il tuo score è: "+score+"/50"
 console.log(score);
