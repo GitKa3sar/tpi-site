075a062771ac258ebdc884a8b22510d0811f4a11
GitKa3sar <106200957+GitKa3sar@users.noreply.github.com>
2023-05-30 17:39:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+//DOM
+const form = document.querySelector("form")
+const submit = document.querySelector("#btn_sub")
+
+
+form.addEventListener("submit" , (e) => {
+    e.preventDefault()
+    const d1 = document.querySelector("input[name='domanda_1']")
+    console.log(d1);
+})
