11136893f60c41a9668a1f9589002665b100e97d
Ka3sar <kaesaryt@gmail.com>
2023-05-30 18:28:49 +0200

    s

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
 const form = document.querySelector("form");
 const submit = document.querySelector("#btn_sub");
 
+ 
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+    let score = 0;
 
   const answers = document.querySelectorAll("input[type='radio']:checked");
 
   answers.forEach((element) => {
     console.log(element.name + " " + element.value);
+    score += element.value;
+
   });
+  console.log(score);
 });
